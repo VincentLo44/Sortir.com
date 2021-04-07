7a9607558cf8e1ffd3ddd3e42ceeb98e8717c334
Mamou-87 <amauryprevost@hotmail.fr>
2021-04-07 11:34:22 +0200

    Home, base.html.twig - starting CSS

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,8 +5,20 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class HomeController
+ * @package App\Controller
+ * @Route(name="home_")
+ */
 class HomeController extends AbstractController
 {
 
+    /**
+     * @Route(path="", name="home",methods={"GET"})
+     */
+    public function home(){
+        return $this->render("home/Home.html.twig");
+    }
 }
