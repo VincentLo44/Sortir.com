3850b7dfe7e86254a5697903ed5af37e5e593c9b
Rose Dorleans <rosedorleans1@gmail.com>
2021-04-08 11:40:32 +0200

    afficher les sorties

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,10 @@
 namespace App\Controller;
 
 
+use App\Entity\Outing;
+use App\Entity\Serie;
+use App\Repository\OutingRepository;
+use http\Env\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,9 +20,20 @@ class HomeController extends AbstractController
 {
 
     /**
-     * @Route(path="", name="home",methods={"GET"})
+     * @Route(path="", name="home", requirements={"numPage":"\d+"}, methods={"GET"})
      */
-    public function home(){
-        return $this->render("home/home.html.twig");
+    public function home( OutingRepository $outingRepository): \Symfony\Component\HttpFoundation\Response
+    {
+
+        $outings = $outingRepository->findAll();
+
+
+        return $this->render('home/home.html.twig', [
+            'outings'=>$outings
+        ]);
     }
+
+
+
+
 }
