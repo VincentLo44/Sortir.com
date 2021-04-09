4a75746953d49a5117af8b8dbad7b34077d8564d
Panda <vincent.loreau4@gmail.com>
2021-04-09 09:52:07 +0200

    modif add.html.twig (div mise en page)

--- src/Form/OutingType.php
+++ src/Form/OutingType.php
@@ -4,7 +4,6 @@ namespace App\Form;
 
 use App\Entity\Outing;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
