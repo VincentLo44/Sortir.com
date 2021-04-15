0fcab2c3e6f018733d1b9b74f736a26af3e5a56a
Mamou-87 <amauryprevost@hotmail.fr>
2021-04-15 10:14:17 +0200

    petites maj sur register

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,20 +5,18 @@ namespace App\Controller;
 use App\Entity\Campus;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
-use App\Security\Authenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 
 class RegistrationController extends AbstractController
 {
     /**
      * @Route("/register", name="app_register")
      */
-    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, Authenticator $authenticator): Response
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
     {
         $user = new User();
         $user->setIsActive(true);
@@ -28,7 +26,7 @@ class RegistrationController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
+
             $user->setCampus($this->getDoctrine()->getRepository(Campus::class)->find($_POST['campus']));
             $user->setPassword(
                 $passwordEncoder->encodePassword(
@@ -40,14 +38,6 @@ class RegistrationController extends AbstractController
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
-            // do anything else you need here, like send an email
-
-//            return $guardHandler->authenticateUserAndHandleSuccess(
-//                $user,
-//                $request,
-//                $authenticator,
-//                'main' // firewall name in security.yaml
-//            );
 
             $this->addFlash('success', 'Congrats ! The account of '.$user->getUsername(). ' has been created !');
 
